06263ea5e89e1bb87725de1f398c06278d92256c
Conor <conorjbyrne228@gmail.com>
2024-09-26 21:54:07 -0400

    I changed fucking nothing and now the code works. why

--- assignments/assignment2/assets/fragmentShader.frag
+++ assignments/assignment2/assets/fragmentShader.frag
@@ -7,6 +7,8 @@ in vec2 TexCoord;
 uniform float uTime;
     //uniform vec4 uColor = vec3(1.0);
 
+//uniform sampler2D ourTexture;
+
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
@@ -16,7 +18,7 @@ void main()
 	//FragColor = Color * (cos(uTime) * 0.5 + 0.5);
 
    // FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);
- //  FragColor = texture(ourTexture, TexCoord);
+  // FragColor = texture(ourTexture, TexCoord);
 
    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 } 
--- assignments/assignment2/main.cpp
+++ assignments/assignment2/main.cpp
@@ -12,13 +12,13 @@ const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
 float vertices[] = {
-//   X     Y     Z			 R     G     B     A        texture coords
+	//   X     Y     Z			 R     G     B     A        texture coords
 
-	0.5f, 0.5f, 0.0f,		1.0f, 0.0f, 0.0f, 1.0f,		1.0f, 1.0f,    // top right
-	0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f, 1.0f,		1.0f, 0.0f,    // bottom right
-	-0.5f,  -0.5f, 0.0f,	0.0f, 0.0f, 1.0f, 1.0f,		0.0f, 0.0f,    // top left 
-	-0.5f, 0.5f, 0.0f,		0.0f, 0.0f, 1.0f, 1.0f,		0.0f, 1.0f     // bottom left
-	
+		0.5f, 0.5f, 0.0f,		1.0f, 0.0f, 0.0f, 1.0f,		1.0f, 1.0f,    // top right
+		0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f, 1.0f,		1.0f, 0.0f,    // bottom right
+		-0.5f,  -0.5f, 0.0f,	0.0f, 0.0f, 1.0f, 1.0f,		0.0f, 0.0f,    // top left 
+		-0.5f, 0.5f, 0.0f,		0.0f, 0.0f, 1.0f, 1.0f,		0.0f, 1.0f     // bottom left
+
 };
 
 unsigned int indices[] = { // note that we start from 0!
@@ -49,17 +49,18 @@ int main() {
 
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
-	
+	//copy our index array in a element buffer for OpenGL to use
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	//Vertex buffer object
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	//copy our index array in a element buffer for OpenGL to use
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	
+
+
 	//Position XYZ
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -69,7 +70,7 @@ int main() {
 	glEnableVertexAttribArray(1);
 
 	// TEXTURE
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(6 * sizeof(float)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(7 * sizeof(float)));
 	glEnableVertexAttribArray(2);
 
 	Shader myShader("assets/vertexShader.vert", "assets/fragmentShader.frag");
@@ -80,23 +81,23 @@ int main() {
 	glGenTextures(1, &texture1);
 	glBindTexture(GL_TEXTURE_2D, texture1);
 
-	unsigned int texture2;
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
 	
+
 	// set the texture wrapping/filtering options (on the currently bound texture object)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	
+
 	// load and generate the texture
+	stbi_set_flip_vertically_on_load(true);
 	int width, height, nrChannels;
 	unsigned char* data = stbi_load("assets/emoji.png", &width, &height, &nrChannels, 0);
 	if (data)
 	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
+		printf("texture 1 loaded\n");
 	}
 	else
 	{
@@ -105,11 +106,16 @@ int main() {
 
 	stbi_image_free(data);
 
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
 	data = stbi_load("assets/winston.png", &width, &height, &nrChannels, 0);
 	if (data)
 	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
+		printf("texture 2 loaded\n");
 	}
 	else
 	{
@@ -128,7 +134,7 @@ int main() {
 
 
 		//DRAW
-		
+
 		//Clear framebuffer
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -142,7 +148,7 @@ int main() {
 
 
 		glBindVertexArray(VAO); //this happens 2x for clarity's sake
-		
+
 		//DRAW CALL
 
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
